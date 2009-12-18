329ae46cd9c397b9f09d742c25d0d988696bd3d1
Yuanle Song <sylecn@gmail.com>
2009-12-18 07:04:36 -0600

    updated README

---
+++ smp.el
@@ -0,0 +1,32 @@
+;;; smp.el --- simple music player
+
+;; Copyright (C) 2009  Yuanle Song
+
+;; Author: Yuanle Song <sylecn@gmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defvar smp-root-dir (concat user-emacs-directory smp)
+  "dir to store smp play lists and config files.")
+
+(provide 'smp)
+
+;;; smp.el ends here
